08211fb943656f1eebd728df29eb97a7aa9fc910
fuyofulo <pheonixdiaz625@gmail.com>
2024-05-20 11:41:14 +0530

    prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,27 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 
 const app = new Hono()
 
-app.post('/api/v1/user/signup', (c) => {
+app.post('/api/v1/user/signup', async (c) => {
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+  const body = await c.req.json();  
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password
+    }
+  })
+
+  // const token = sign({ id: user.id})
+
   return c.text('signup route')
 })
 
