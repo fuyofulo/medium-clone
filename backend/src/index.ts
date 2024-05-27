b07b205c1516a5e3a66a5f8b635cf38accb24498
fuyofulo <pheonixdiaz625@gmail.com>
2024-05-28 01:20:40 +0530

    some changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,31 +3,76 @@ import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { decode, sign, verify } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    SECRET: string,
+  }
+}>();
 
-app.post('/api/v1/user/signup', async (c) => {
+app.post('/api/v1/signup', async (c) => {
+
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+    }
+  })
+
+  if(existingUser) {
+    c.status(400)
+    return c.json({ error: 'Email already exists' });
+  }
+
+  try {
+    const user = await prisma.user.create({ data: { email: body.email, password: body.password } })
+    if(!user) { 
+        return c.text('user not created') 
+    }
+    console.log('SECRET:', c.env.SECRET);
+    console.log('DATABASE:', c.env.DATABASE_URL);
+    //@ts-ignore
+    const token = await sign({ id: user.id }, c.env.SECRET) 
+    return c.json({ jwt: token })
+  } catch (error) {
+    console.error(error)
+    if(error) { 
+      return c.text ('unable to generate token')
+    }  
+  }
+})
+
+
+app.post('/api/v1/signin', async (c) => {
+
   const prisma = new PrismaClient({
     //@ts-ignore
     datasourceUrl: env.DATABASE_URL,
     }).$extends(withAccelerate());
 
-  const body = await c.req.json();  
+  const body = await c.req.json();
 
-  const user = await prisma.user.create({
-    data: {
-      email: body.email,
-      password: body.password
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email
     }
   })
 
-  // const token = sign({ id: user.id})
-
-  return c.text('signup route')
-})
-
-
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('signin route')
+  if(!user) {
+    c.status(403);
+    return c.json({
+      error: 'user does not exist'
+    })
+  }
+  //@ts-ignore
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET)
+  return c.json({ jwt }); 
 })
 
 
