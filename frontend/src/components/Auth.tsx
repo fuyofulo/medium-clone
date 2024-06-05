9a775e97beac482a246a3098ff085c72fb22d1a8
fuyofulo <pheonixdiaz625@gmail.com>
2024-06-06 00:39:55 +0530

    some progress

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -9,39 +9,42 @@ const Auth = ({type}: {type: "signup" | "signin"}) => {
         email: "",
         password: ""
     });
-    return <div className="h-screen flex justify-center flex-col">
-        <div className="flex justify-center">
-            <div>
-                <div className="px-10">
-                    <div className="text-3xl font-extrabold">
-                        Create an account
+    return <div className="h-screen flex justify-center">
+        <div className="flex justify-center flex-col w-full lg:w-3/4">
+            <div className="w-full">
+                <div className="px-24">
+                    <div className="text-3xl flex justify-center font-extrabold">
+                        {type === "signup" ? "Create an account" : "Login your account"}
                     </div>
-                    <div className="text-slate-400">
+                    <div className="text-slate-400 flex justify-center">
                           {type === "signup" ? "Already have an account?" : "Don't have an account?"}
-                        <Link to={"/signin"} className="pl-2 underline">Login</Link>
+                        <Link to={type === "signup" ? '/signin' : '/signup'} className="pl-2 underline">
+                            {type === "signup" ? "Login" : "Sign Up"}
+                        </Link>
                     </div>
                 </div>
                 <div className="pt-8">
-                    <LabelledInput label="Name" placeholder="Zaid Khan..." onChange={(e) => {
-                        setPostInputs({
-                            ...postInputs,
-                            name: e.target.value
-                        })
-                    }}></LabelledInput><br />
-                    <LabelledInput label="Email" placeholder="zaidkhan@gmail.com" onChange={(e) => {
-                        setPostInputs({
-                            ...postInputs,
-                            email: e.target.value
-                        })
-                    }}></LabelledInput><br />
-                    <LabelledInput label="Password" type={"password"} placeholder="********" onChange={(e) => {
-                        setPostInputs({
-                            ...postInputs,
-                            password: e.target.value
-                        })
-                    }}></LabelledInput><br />
-                    <button type="button" className="text-white w-full bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign Up" : "Sign In"}</button>
-
+                    <div className="lg:px-14">
+                        {type === "signup" ? <LabelledInput label="Name" placeholder="Zaid Khan..." onChange={(e) => {
+                            setPostInputs({
+                                ...postInputs,
+                                name: e.target.value
+                            })
+                        }}></LabelledInput> : null}
+                        <LabelledInput label="Email" placeholder="zaidkhan@gmail.com" onChange={(e) => {
+                            setPostInputs({
+                                ...postInputs,
+                                email: e.target.value
+                            })
+                        }}></LabelledInput><br />
+                        <LabelledInput label="Password" type={"password"} placeholder="********" onChange={(e) => {
+                            setPostInputs({
+                                ...postInputs,
+                                password: e.target.value
+                            })
+                        }}></LabelledInput><br />
+                        <button type="button" className="text-white w-full bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign Up" : "Login"}</button>
+                    </div>
                 </div>
             </div>
         </div>
